c21fec7e92e4ca71e279bb066152a7edc2896d97
saladjay <dyj394041771@sina.com>
2017-09-29 13:37:55 +0800

    加入專案檔案。

---
+++ XmlReaderAndWriter/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace XmlReaderAndWriter
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SaveFileDialog saveDlg = new SaveFileDialog();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            saveDlg.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+            saveDlg.Filter = "FactoryPresets File|*.xml";
+            saveDlg.OverwritePrompt = true;
+            if (saveDlg.ShowDialog() == true)
+            {
+                string strp = saveDlg.FileName;
+
+                XmlDocument xmlDoc = new XmlDocument();
+                XmlNode node = null;
+                node = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+                XmlNode root = xmlDoc.CreateElement("Config");
+                xmlDoc.AppendChild(root);
+                xmlDoc.Save(strp);
+
+                // BitConverter.ToString(mFactoryData);//for show...
+                // textBx.AppendText("\n");
+                // textBx.AppendText(strp);
+            }
+        }
+    }
+}
---
+++ XmlReaderAndWriter/Xmlhelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlReaderAndWriter
+{
+    public static class Xmlhelper
+    {
+        public static void CreateXML(string Path,List<string> key, List<string> value,bool StandAlone=true)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode node = null;
+            if (StandAlone)
+                node = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            else
+                node = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", "no");
+        }
+    }
+}
